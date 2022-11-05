b77195e79a8130392c20dac1f92c87c7ecec8637
Lennart Koopmann <lennart@0x58ed.com>
2022-11-05 14:56:31 -0500

    New Registry method

--- src/main/java/app/nzyme/plugin/Registry.java
+++ src/main/java/app/nzyme/plugin/Registry.java
@@ -1,10 +1,15 @@
 package app.nzyme.plugin;
 
+import javax.annotation.Nullable;
 import java.util.Optional;
 
 public interface Registry {
 
     Optional<String> getValue(String key);
+
+    @Nullable
+    String getValueOrNull(String key);
+
     void setValue(String key, String value);
 
 }
