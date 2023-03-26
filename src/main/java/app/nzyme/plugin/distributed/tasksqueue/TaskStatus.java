f01514f25d04478bab5c9682e3ccff6c2deb7961
Lennart Koopmann <lennart@0x58ed.com>
2023-03-26 15:47:48 -0500

    Add CANCELED task status

--- src/main/java/app/nzyme/plugin/distributed/tasksqueue/TaskStatus.java
+++ src/main/java/app/nzyme/plugin/distributed/tasksqueue/TaskStatus.java
@@ -2,6 +2,6 @@ package app.nzyme.plugin.distributed.tasksqueue;
 
 public enum TaskStatus {
 
-    NEW, NEW_RETRY, ACK, PROCESSED_SUCCESS, PROCESSED_FAILURE
+    NEW, NEW_RETRY, ACK, PROCESSED_SUCCESS, PROCESSED_FAILURE, CANCELED
 
 }
