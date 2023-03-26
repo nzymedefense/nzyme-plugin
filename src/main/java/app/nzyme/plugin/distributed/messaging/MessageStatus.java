0db6a1a3b50aea79c47eb31c0a17297448466fbe
Lennart Koopmann <lennart@0x58ed.com>
2023-03-26 17:06:31 -0500

    Add additional MessageStatus for ack'd failures

--- src/main/java/app/nzyme/plugin/distributed/messaging/MessageStatus.java
+++ src/main/java/app/nzyme/plugin/distributed/messaging/MessageStatus.java
@@ -2,6 +2,6 @@ package app.nzyme.plugin.distributed.messaging;
 
 public enum MessageStatus {
 
-    NEW, ACK, INVALID, PROCESSED_SUCCESS, PROCESSED_FAILURE
+    NEW, ACK, INVALID, PROCESSED_SUCCESS, PROCESSED_FAILURE, FAILURE_ACKNOWLEDGED
 
 }
