09c2afe54fb425fe15d4e29147ef6ff4b448087f
Lennart Koopmann <lennart@0x58ed.com>
2023-05-25 17:55:06 -0500

    Add EnumStringConstraint

---
+++ src/main/java/app/nzyme/plugin/rest/configuration/EnumStringConstraint.java
@@ -0,0 +1,29 @@
+package app.nzyme.plugin.rest.configuration;
+
+import com.google.auto.value.AutoValue;
+
+import java.util.List;
+
+@AutoValue
+public abstract class EnumStringConstraint {
+
+    public abstract List<String> strings();
+
+    public static EnumStringConstraint create(List<String> strings) {
+        return builder()
+                .strings(strings)
+                .build();
+    }
+
+    public static Builder builder() {
+        return new AutoValue_EnumStringConstraint.Builder();
+    }
+
+    @AutoValue.Builder
+    public abstract static class Builder {
+        public abstract Builder strings(List<String> strings);
+
+        public abstract EnumStringConstraint build();
+    }
+    
+}
