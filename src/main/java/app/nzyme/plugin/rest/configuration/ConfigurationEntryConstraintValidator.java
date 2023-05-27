fead1718e5f2808db199682b912cb09e30ea0902
Lennart Koopmann <lennart@0x58ed.com>
2023-05-27 18:17:55 -0500

    Make ConfigurationEntryConstraintValidator work with EncryptedRegistryKey

--- src/main/java/app/nzyme/plugin/rest/configuration/ConfigurationEntryConstraintValidator.java
+++ src/main/java/app/nzyme/plugin/rest/configuration/ConfigurationEntryConstraintValidator.java
@@ -1,5 +1,6 @@
 package app.nzyme.plugin.rest.configuration;
 
+import app.nzyme.plugin.EncryptedRegistryKey;
 import app.nzyme.plugin.RegistryKey;
 
 import java.util.List;
@@ -9,7 +10,14 @@ import java.util.Optional;
 public class ConfigurationEntryConstraintValidator {
 
     public static boolean checkConstraints(RegistryKey key, Map.Entry<String, Object> variable) {
-        Optional<List<ConfigurationEntryConstraint>> constraints = key.constraints();
+        return checkConstraints(key.constraints(), variable);
+    }
+
+    public static boolean checkConstraints(EncryptedRegistryKey key, Map.Entry<String, Object> variable) {
+        return checkConstraints(key.constraints(), variable);
+    }
+
+    private static boolean checkConstraints(Optional<List<ConfigurationEntryConstraint>> constraints, Map.Entry<String, Object> variable) {
         if (constraints.isPresent()) {
             for (ConfigurationEntryConstraint constraint : constraints.get()) {
                 ConstraintValidationResult check = ConstraintValidator.validate(variable.getValue(), constraint);
