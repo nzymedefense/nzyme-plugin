a4404ce397f1a1c3a6d0e6b50aa21070e9b526f0
Lennart Koopmann <lennart.koopmann@nzyme.org>
2023-12-01 15:17:06 -0600

    Add INVALIDATE_CACHE message type

--- src/main/java/app/nzyme/plugin/distributed/messaging/MessageType.java
+++ src/main/java/app/nzyme/plugin/distributed/messaging/MessageType.java
@@ -3,6 +3,7 @@ package app.nzyme.plugin.distributed.messaging;
 public enum MessageType {
 
     CHECK_RESTART_HTTP_SERVER,
-    CLUSTER_PGP_KEYS_PROVIDED
+    CLUSTER_PGP_KEYS_PROVIDED,
+    INVALIDATE_CACHE
 
 }
