99f25861eb566e1cf9270cd0e9720cf7feb0b1b0
Lennart Koopmann <lennart.koopmann@nzyme.org>
2024-11-22 13:14:18 -0600

    Add new task types

--- src/main/java/app/nzyme/plugin/distributed/tasksqueue/TaskType.java
+++ src/main/java/app/nzyme/plugin/distributed/tasksqueue/TaskType.java
@@ -2,6 +2,10 @@ package app.nzyme.plugin.distributed.tasksqueue;
 
 public enum TaskType {
 
-    PROVIDE_PGP_KEYS, TEST
+    PROVIDE_PGP_KEYS,
+    TEST,
+    PURGE_DATA_CATEGORY_GLOBAL,
+    PURGE_DATA_CATEGORY_ORGANIZATION,
+    PURGE_DATA_CATEGORY_TENANT
 
 }
